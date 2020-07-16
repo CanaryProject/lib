aaad2f513caf08f5c22b9444555dfcce7388f7f1
Lucas Grossi <lucas.ggrossi@gmail.com>
2020-07-16 15:13:24 -0300

    better .sh scripts.

--- generate_messages.sh
+++ generate_messages.sh
@@ -1,5 +1,20 @@
-cd external/flatbuffers
-cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release
-make
-cd ../..
-./external/flatbuffers/flatc -c -o protocol/messages/ flatbuffers/sample.fbs
+#!/usr/bin/env bash
+BUILD="Off"
+
+while getopts "b" opt; do
+  case "$opt" in
+    b) BUILD="On" ;;
+  esac
+done
+
+if [ ${BUILD} == "On" ]; then
+  ./canary_echo.sh "Building flatbuffers dependencies"
+  cd external/flatbuffers
+  cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release
+  make
+  cd ../..
+fi
+
+./canary_echo.sh "Generating flatbuffers messages"
+./external/flatbuffers/flatc -c -o protocol/messages/ flatbuffers/*
+./canary_echo.sh "Finished"
