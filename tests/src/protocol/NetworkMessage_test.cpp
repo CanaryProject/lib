1156f6e160289afab8c92918c01380e505124dce
Lucas Grossi <lucas.ggrossi@gmail.com>
2020-07-19 09:11:15 -0300

    [UnitTests]: cover network_message with tests.

--- tests/src/protocol/NetworkMessage_test.cpp
+++ tests/src/protocol/NetworkMessage_test.cpp
@@ -25,6 +25,85 @@ TEST_SUITE("NetworkMessage Test") {
     uint32_t value = 120000;
     validateMessage<uint32_t>(value);
   }
+  TEST_CASE("NetworkMessage read/write byte") {
+    CanaryLib::NetworkMessage msg;
+    uint8_t value = 255;
+    msg.writeByte(value);
+    msg.setBufferPosition(CanaryLib::MAX_HEADER_SIZE);
+    CHECK_EQ(msg.readByte(), value);
+  }
+  TEST_CASE("NetworkMessage read/write string") {
+    CanaryLib::NetworkMessage msg;
+    std::string value = "myString";
+    msg.writeString(value);
+    msg.setBufferPosition(CanaryLib::MAX_HEADER_SIZE);
+    CHECK_EQ(msg.readString(), value);
+  }
+  TEST_CASE("NetworkMessage write bytes") {
+    std::string name = "myString";
+    uint32_t id = 3741277123;
+
+    CanaryLib::NetworkMessage msg;
+    msg.writeString(name);
+    msg.write<uint32_t>(id);
+    msg.setBufferPosition(CanaryLib::MAX_HEADER_SIZE);
+    
+    CanaryLib::NetworkMessage output;
+    output.write(msg.getOutputBuffer(), msg.getLength(), CanaryLib::MESSAGE_INCREMENT_SIZE);
+    CHECK_EQ(output.readString(), name);
+    CHECK_EQ(output.read<uint32_t>(), id);
+  }
+  TEST_CASE("NetworkMessage write increment flag") {
+    uint32_t id = 3741277123;
+
+    CanaryLib::NetworkMessage msg;
+    msg.write<uint32_t>(id);
+    msg.setBufferPosition(CanaryLib::MAX_HEADER_SIZE);
+    
+    CanaryLib::NetworkMessage output;
+    output.write(msg.getOutputBuffer(), msg.getLength(), CanaryLib::MESSAGE_INCREMENT_SIZE);
+    CHECK_EQ(output.getBufferPosition(), CanaryLib::MAX_HEADER_SIZE);
+    CHECK_EQ(output.getLength(), sizeof(uint32_t));
+    output.reset();
+    output.write(msg.getOutputBuffer(), msg.getLength(), CanaryLib::MESSAGE_INCREMENT_BUFFER);
+    CHECK_EQ(output.getBufferPosition(), CanaryLib::MAX_HEADER_SIZE + sizeof(uint32_t));
+    CHECK_EQ(output.getLength(), 0);
+    output.reset();
+    output.write(msg.getOutputBuffer(), msg.getLength(), CanaryLib::MESSAGE_INCREMENT_BUFFER_AND_SIZE);
+    CHECK_EQ(output.getBufferPosition(), CanaryLib::MAX_HEADER_SIZE + sizeof(uint32_t));
+    CHECK_EQ(output.getLength(), sizeof(uint32_t));
+  }
+  TEST_CASE("NetworkMessage writePaddingBytes") {
+    CanaryLib::NetworkMessage msg;
+    uint8_t n = 8;
+    for (uint8_t i = 0; i <= n; i++) {
+      msg.reset();
+      msg.writePaddingBytes(i);
+      CHECK_EQ(msg.getLength(), i);
+    }
+    CHECK_EQ(msg.getBufferPosition(), CanaryLib::MAX_HEADER_SIZE);
+  }
+  TEST_CASE("NetworkMessage skip") {
+    CanaryLib::NetworkMessage msg;
+    uint8_t n = 8;
+    msg.skip(n);
+    CHECK_EQ(msg.getBufferPosition(), CanaryLib::MAX_HEADER_SIZE + n);
+  }
+  TEST_CASE("NetworkMessage skip<T>") {
+    CanaryLib::NetworkMessage msg;
+    msg.skip<double>();
+    CHECK_EQ(msg.getBufferPosition(), CanaryLib::MAX_HEADER_SIZE + sizeof(double));
+    msg.reset();
+    msg.skip<uint8_t>();
+    CHECK_EQ(msg.getBufferPosition(), CanaryLib::MAX_HEADER_SIZE + sizeof(uint8_t));
+    msg.reset();
+    msg.skip<uint16_t>();
+    CHECK_EQ(msg.getBufferPosition(), CanaryLib::MAX_HEADER_SIZE + sizeof(uint16_t));
+    msg.reset();
+    msg.skip<uint32_t>();
+    CHECK_EQ(msg.getBufferPosition(), CanaryLib::MAX_HEADER_SIZE + sizeof(uint32_t));
+    msg.reset();
+  }
   TEST_CASE("Flatbuffer input/output test") {
     std::string nameStr = "myName";
     uint32_t id = 3294967295;
