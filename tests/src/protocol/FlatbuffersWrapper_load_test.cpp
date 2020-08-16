4c02138378d26fc812a2f67db73e5544b7a0ad3d
Lucas Grossi <lucas.ggrossi@gmail.com>
2020-08-16 17:01:40 +0200

    Add load test for flatbuffers

---
+++ tests/src/protocol/FlatbuffersWrapper_load_test.cpp
@@ -0,0 +1,164 @@
+#include "../all.h"
+
+namespace FlatbuffersWrapperLoadTest {
+  TEST_SUITE("FlatbuffersWrapper LoadTest") {
+    CanaryLib::XTEA xtea;
+    std::string nameStr("myName");
+    uint32_t id32 = 32412516;
+    uint16_t id16 = 65000;
+    uint8_t dmg = 123;
+
+    void measureExecution(std::string name, std::function<void()> load) {
+      auto started = std::chrono::high_resolution_clock::now();
+      load();
+      auto done = std::chrono::high_resolution_clock::now();
+      spdlog::info("Process \"{}\" executed in {}ms", name, std::chrono::duration_cast<std::chrono::milliseconds>(done-started).count());
+    }
+
+    void loadTestNoFlatbuffer(int nMessages, int msgSize) {
+      int msgLength = msgSize * 15;
+      CanaryLib::NetworkMessage bigMsg;
+      for (int i = 0; i < nMessages; i++) {
+        CanaryLib::NetworkMessage msg;
+        for (int j = 0; j < msgSize; j++) {
+          msg.writeString(nameStr);
+          msg.write<uint8_t>(dmg);
+          msg.write<uint16_t>(id16);
+          msg.write<uint32_t>(id32);
+        }
+        bigMsg.write(msg.getBuffer(), msg.getLength());
+      }
+
+      for (int i = 0; i < nMessages; i++) {
+        CanaryLib::NetworkMessage output;
+        output.write(bigMsg.getBuffer() + (i * msgLength), msgLength, CanaryLib::MESSAGE_OPERATION_PEEK);
+        for (int j = 0; j < msgSize; j++) {
+          CHECK_EQ(output.readString(), nameStr);
+          CHECK_EQ(output.read<uint8_t>(), dmg);
+          CHECK_EQ(output.read<uint16_t>(), id16);
+          CHECK_EQ(output.read<uint32_t>(), id32);
+          CHECK_EQ(output.getLength(), msgLength);
+        }
+      }
+    }
+
+    void loadTestRawDataMessage(int nMessages, int msgSize) {
+      CanaryLib::FlatbuffersWrapper wrapper1;
+      flatbuffers::FlatBufferBuilder &fbb = wrapper1.Builder();
+
+      for (int i = 0; i < nMessages; i++) {
+        CanaryLib::NetworkMessage msg;
+        for (int j = 0; j < msgSize; j++) {
+          msg.writeString(nameStr);
+          msg.write<uint8_t>(dmg);
+          msg.write<uint16_t>(id16);
+          msg.write<uint32_t>(id32);
+        }
+        auto buffer = fbb.CreateVector(msg.getBuffer(), msg.getLength());
+        auto raw_data = CanaryLib::CreateRawData(fbb, buffer, msg.getLength());
+        wrapper1.add(raw_data.Union(), CanaryLib::DataType_RawData);
+      }
+      wrapper1.Finish();
+      
+      CHECK_EQ(wrapper1.Size(), fbb.GetSize());
+      CHECK(wrapper1.readChecksum());
+
+      auto enc_msg = wrapper1.getEncryptedMessage();
+      CHECK_FALSE(enc_msg->header()->encrypted());
+
+      uint8_t *body_buffer = (uint8_t *) enc_msg->body()->Data();
+      auto content_msg = CanaryLib::GetContentMessage(body_buffer);
+      for (int i = 0; i < nMessages; i++) {
+        CanaryLib::NetworkMessage msg;
+        CHECK_EQ(content_msg->data_type()->GetEnum<CanaryLib::DataType>(i), CanaryLib::DataType_RawData);
+        auto raw_data = content_msg->data()->GetAs<CanaryLib::RawData>(i);
+        msg.write(raw_data->body()->data(), raw_data->size(), CanaryLib::MESSAGE_OPERATION_PEEK);
+        CHECK_EQ(msg.getLength(), raw_data->size());
+        for (int j = 0; j < msgSize; j++) {
+          CHECK_EQ(msg.readString(), nameStr);
+          CHECK_EQ(msg.read<uint8_t>(), dmg);
+          CHECK_EQ(msg.read<uint16_t>(), id16);
+          CHECK_EQ(msg.read<uint32_t>(), id32);
+        }
+      }
+    }
+
+    void loadTestWeaponMessage(int nMessages) {
+      CanaryLib::FlatbuffersWrapper wrapper1;
+      flatbuffers::FlatBufferBuilder &fbb = wrapper1.Builder();
+
+      for (int i = 0; i < nMessages; i++) {
+        auto name2 = fbb.CreateString(nameStr);
+        auto weapon_data = CanaryLib::CreateWeaponData(fbb, id16, name2, dmg);
+        wrapper1.add(weapon_data.Union(), CanaryLib::DataType_WeaponData);
+      }
+      wrapper1.Finish();
+      
+      CHECK_EQ(wrapper1.Size(), fbb.GetSize());
+      CHECK(wrapper1.readChecksum());
+
+      auto enc_msg = wrapper1.getEncryptedMessage();
+      CHECK_FALSE(enc_msg->header()->encrypted());
+
+      uint8_t *body_buffer = (uint8_t *) enc_msg->body()->Data();
+      auto content_msg = CanaryLib::GetContentMessage(body_buffer);
+      for (int i = 0; i < nMessages; i++) {
+        CHECK_EQ(content_msg->data_type()->GetEnum<CanaryLib::DataType>(i), CanaryLib::DataType_WeaponData);
+        auto weapon = content_msg->data()->GetAs<CanaryLib::WeaponData>(1);
+        CHECK_EQ(weapon->name()->str(), nameStr);
+        CHECK_EQ(weapon->id(), id16);
+        CHECK_EQ(weapon->damage(), dmg);
+      }
+    }
+
+    void loadExecution(CanaryLib::DataType type, int messages, int msgSize = 1, int executions = 100) {
+      auto name = std::string("(")
+        .append(std::to_string(executions))
+        .append("x, ").append(std::to_string(messages))
+        .append(" messages with size ").append(std::to_string(msgSize))
+        .append(") MultipleSmallMessages - LoadTest");
+
+      switch (type) {
+        case CanaryLib::DataType_RawData:
+          measureExecution(name, [=]() { 
+            for (int i = 0; i < executions; i++)
+              loadTestRawDataMessage(messages, msgSize); 
+          });
+          return;
+        case CanaryLib::DataType_WeaponData:
+          measureExecution(name, [=]() { 
+            for (int i = 0; i < executions; i++)
+              loadTestWeaponMessage(messages); 
+          });
+          return;
+        case CanaryLib::DataType_NONE:
+        default:
+          measureExecution(name, [=]() { 
+            for (int i = 0; i < executions; i++)
+              loadTestNoFlatbuffer(messages, msgSize); 
+          });
+          return;
+      }
+    }
+
+    TEST_CASE("Load test, multiple small RawData") {
+      loadExecution(CanaryLib::DataType_RawData, 1740);
+    }
+
+    TEST_CASE("Load test, one big message RawData") {
+      loadExecution(CanaryLib::DataType_RawData, 1, 3800);
+    }
+
+    TEST_CASE("Load test, multiple small NetworkMessages") {
+      loadExecution(CanaryLib::DataType_NONE, 3800);
+    }
+
+    TEST_CASE("Load test, one big NetworkMessages") {
+      loadExecution(CanaryLib::DataType_NONE, 1, 3800);
+    }
+
+    TEST_CASE("Load test, multiple weapon data") {
+      loadExecution(CanaryLib::DataType_WeaponData, 2250);
+    }
+  }
+}
--- tests/src/protocol/FlatbuffersWrapper_test.cpp
+++ tests/src/protocol/FlatbuffersWrapper_test.cpp
@@ -1,181 +1,155 @@
 #include "../all.h"
 
-TEST_SUITE("FlatbuffersWrapper") {
-  CanaryLib::XTEA xtea;
-  std::string nameStr("myName");
-  std::string rawStr("rawMsg");
-  uint32_t id32 = 32412516;
-  uint16_t id16 = 65000;
-  uint8_t dmg = 123;
+namespace FlatbuffersWrapperTest {
+  TEST_SUITE("FlatbuffersWrapper") {
+    CanaryLib::XTEA xtea;
+    std::string nameStr("myName");
+    std::string rawStr("rawMsg");
+    uint32_t id32 = 32412516;
+    uint16_t id16 = 65000;
+    uint8_t dmg = 123;
 
-  TEST_CASE("Load test, should work fine with up to 2^16 bytes msg and wrappers") {
-    CanaryLib::FlatbuffersWrapper wrapper1;
-    CanaryLib::FlatbuffersWrapper wrapper2;
-    flatbuffers::FlatBufferBuilder &fbb = wrapper1.Builder();
+    void generateBaseWrapper(CanaryLib::FlatbuffersWrapper &wrapper1) {
+      xtea.generateKey();
+      flatbuffers::FlatBufferBuilder &fbb = wrapper1.Builder();
 
-    for (int i = 0; i < 10; i++) {
-      CanaryLib::NetworkMessage msg;
-      for (int j = 0; j < 100; j++) {
-        msg.write<uint8_t>(12);
-        msg.write<uint16_t>(12);
-        msg.write<uint32_t>(12);
-      }
-      auto buffer = fbb.CreateVector(msg.getBuffer(), msg.getLength());
-      auto raw_data = CanaryLib::CreateRawData(fbb, buffer, msg.getLength());
+      auto name = fbb.CreateString(nameStr);
+      auto player_data = CanaryLib::CreatePlayerData(fbb, id32, name);
+      fbb.Finish(player_data);
+      wrapper1.add(player_data.Union(), CanaryLib::DataType_PlayerData);
+
+      auto name2 = fbb.CreateString(nameStr);
+      auto weapon_data = CanaryLib::CreateWeaponData(fbb, id16, name2, dmg);
+      fbb.Finish(weapon_data);
+      wrapper1.add(weapon_data.Union(), CanaryLib::DataType_WeaponData);
+
+      auto buffer = fbb.CreateVector((uint8_t *) rawStr.c_str(), rawStr.size());
+      auto raw_data = CanaryLib::CreateRawData(fbb, buffer, rawStr.size());
+      fbb.Finish(raw_data);
       wrapper1.add(raw_data.Union(), CanaryLib::DataType_RawData);
-      spdlog::critical("SIZE PORRA {}", fbb.GetSize());
     }
-    wrapper2.copy(wrapper1.Finish());
-    spdlog::critical("SIZE PORRA {} {}", wrapper2.Size(), fbb.GetSize());
-    CHECK(wrapper1.readChecksum());
-    CHECK_EQ(wrapper2.Size(), wrapper1.Size());
-    
-    auto cs1 =  CanaryLib::FlatbuffersWrapper::getChecksum(wrapper1.getEncryptedMessage()->body()->data(), wrapper1.getEncryptedMessage()->header()->message_size());
-    auto cs2 =  CanaryLib::FlatbuffersWrapper::getChecksum(wrapper2.getEncryptedMessage()->body()->data(), wrapper2.getEncryptedMessage()->header()->message_size());
-    CHECK_EQ(cs1, cs2);
-    CHECK(wrapper2.readChecksum());
-  }
 
-  void generateBaseWrapper(CanaryLib::FlatbuffersWrapper &wrapper1) {
-    xtea.generateKey();
-    flatbuffers::FlatBufferBuilder &fbb = wrapper1.Builder();
+    void validateBasic(CanaryLib::FlatbuffersWrapper &wrapper1, CanaryLib::XTEA* _xtea = nullptr) {
+      auto enc_msg = wrapper1.getEncryptedMessage();
 
-    auto name = fbb.CreateString(nameStr);
-    auto player_data = CanaryLib::CreatePlayerData(fbb, id32, name);
-    fbb.Finish(player_data);
-    wrapper1.add(player_data.Union(), CanaryLib::DataType_PlayerData);
+      CHECK_EQ(wrapper1.isWriteLocked(), true);
+      CHECK(wrapper1.readChecksum());
 
-    auto name2 = fbb.CreateString(nameStr);
-    auto weapon_data = CanaryLib::CreateWeaponData(fbb, id16, name2, dmg);
-    fbb.Finish(weapon_data);
-    wrapper1.add(weapon_data.Union(), CanaryLib::DataType_WeaponData);
+      uint8_t *body_buffer = (uint8_t *) enc_msg->body()->Data();
 
-    auto buffer = fbb.CreateVector((uint8_t *) rawStr.c_str(), rawStr.size());
-    auto raw_data = CanaryLib::CreateRawData(fbb, buffer, rawStr.size());
-    fbb.Finish(raw_data);
-    wrapper1.add(raw_data.Union(), CanaryLib::DataType_RawData);
-  }
+      if (_xtea)
+        _xtea->decrypt(enc_msg->header()->message_size(), body_buffer);
 
-  void validateBasic(CanaryLib::FlatbuffersWrapper &wrapper1, CanaryLib::XTEA* _xtea = nullptr) {
-    auto enc_msg = wrapper1.getEncryptedMessage();
+      auto content_msg = CanaryLib::GetContentMessage(body_buffer);
 
-    CHECK_EQ(wrapper1.isWriteLocked(), true);
-    CHECK(wrapper1.readChecksum());
+      CHECK_EQ(enc_msg->header()->encrypted(), !!_xtea);
+      CHECK_EQ(content_msg->data()->size(), 3);
 
-    uint8_t *body_buffer = (uint8_t *) enc_msg->body()->Data();
+      CHECK_EQ(content_msg->data_type()->GetEnum<CanaryLib::DataType>(0), CanaryLib::DataType_PlayerData);
+      CHECK_EQ(content_msg->data_type()->GetEnum<CanaryLib::DataType>(1), CanaryLib::DataType_WeaponData);
+      CHECK_EQ(content_msg->data_type()->GetEnum<CanaryLib::DataType>(2), CanaryLib::DataType_RawData);
 
-    if (_xtea)
-      _xtea->decrypt(enc_msg->header()->message_size(), body_buffer);
+      auto player = content_msg->data()->GetAs<CanaryLib::PlayerData>(0);
+      CHECK_EQ(player->name()->str(), nameStr);
+      CHECK_EQ(player->id(), id32);
 
-    auto content_msg = CanaryLib::GetContentMessage(body_buffer);
+      auto weapon = content_msg->data()->GetAs<CanaryLib::WeaponData>(1);
+      CHECK_EQ(weapon->name()->str(), nameStr);
+      CHECK_EQ(weapon->id(), id16);
+      CHECK_EQ(weapon->damage(), dmg);
 
-    CHECK_EQ(enc_msg->header()->encrypted(), !!_xtea);
-    CHECK_EQ(content_msg->data()->size(), 3);
+      auto raw = content_msg->data()->GetAs<CanaryLib::RawData>(2);
+      
+      CHECK_EQ(std::string((char *) raw->body()->data()), rawStr);
+      CHECK_EQ(raw->size(), rawStr.size());
+    }
 
-    CHECK_EQ(content_msg->data_type()->GetEnum<CanaryLib::DataType>(0), CanaryLib::DataType_PlayerData);
-    CHECK_EQ(content_msg->data_type()->GetEnum<CanaryLib::DataType>(1), CanaryLib::DataType_WeaponData);
-    CHECK_EQ(content_msg->data_type()->GetEnum<CanaryLib::DataType>(2), CanaryLib::DataType_RawData);
+    TEST_CASE("Copy") {
+      CanaryLib::FlatbuffersWrapper wrapper1;
+      CanaryLib::FlatbuffersWrapper wrapper2;
+      generateBaseWrapper(wrapper1);
+      wrapper2.copy(wrapper1.Finish());
+      validateBasic(wrapper2);
+      CHECK_EQ(wrapper2.Size(), wrapper1.Size());
+      wrapper2.copy(wrapper1.Finish() + CanaryLib::WRAPPER_HEADER_SIZE, wrapper1.Size());
+      validateBasic(wrapper2);
+      CHECK_EQ(wrapper2.Size(), wrapper1.Size());
+    }
 
-    auto player = content_msg->data()->GetAs<CanaryLib::PlayerData>(0);
-    CHECK_EQ(player->name()->str(), nameStr);
-    CHECK_EQ(player->id(), id32);
+    TEST_CASE("Test with NetworkMessage") {
+      CanaryLib::FlatbuffersWrapper wrapper;
+      
+      CanaryLib::NetworkMessage msg;
+      flatbuffers::FlatBufferBuilder &fbb = wrapper.Builder();
+      auto fbuffer = fbb.CreateVector(msg.getBuffer(), msg.getLength());
+      auto raw_data = CanaryLib::CreateRawData(fbb, fbuffer, msg.getLength());
+      fbb.Finish(raw_data);
+      wrapper.add(raw_data.Union(), CanaryLib::DataType_RawData);
 
-    auto weapon = content_msg->data()->GetAs<CanaryLib::WeaponData>(1);
-    CHECK_EQ(weapon->name()->str(), nameStr);
-    CHECK_EQ(weapon->id(), id16);
-    CHECK_EQ(weapon->damage(), dmg);
+      CanaryLib::FlatbuffersWrapper inputWrapper;
+      wrapper.Finish();
+      inputWrapper.copy(wrapper.Finish());
 
-    auto raw = content_msg->data()->GetAs<CanaryLib::RawData>(2);
-    
-    CHECK_EQ(std::string((char *) raw->body()->data()), rawStr);
-    CHECK_EQ(raw->size(), rawStr.size());
-  }
+      // validate checksum
+      bool checksummed = inputWrapper.readChecksum();
 
-  TEST_CASE("Copy") {
-    CanaryLib::FlatbuffersWrapper wrapper1;
-    CanaryLib::FlatbuffersWrapper wrapper2;
-    generateBaseWrapper(wrapper1);
-    wrapper2.copy(wrapper1.Finish());
-    validateBasic(wrapper2);
-    CHECK_EQ(wrapper2.Size(), wrapper1.Size());
-    wrapper2.copy(wrapper1.Finish() + CanaryLib::WRAPPER_HEADER_SIZE, wrapper1.Size());
-    validateBasic(wrapper2);
-    CHECK_EQ(wrapper2.Size(), wrapper1.Size());
-  }
+      auto enc_msg = inputWrapper.getEncryptedMessage();
+      uint8_t *body_buffer = (uint8_t *) enc_msg->body()->Data();
 
-  TEST_CASE("Test with NetworkMessage") {
-    CanaryLib::FlatbuffersWrapper wrapper;
-    
-    CanaryLib::NetworkMessage msg;
-    flatbuffers::FlatBufferBuilder &fbb = wrapper.Builder();
-    auto fbuffer = fbb.CreateVector(msg.getBuffer(), msg.getLength());
-    auto raw_data = CanaryLib::CreateRawData(fbb, fbuffer, msg.getLength());
-    fbb.Finish(raw_data);
-    wrapper.add(raw_data.Union(), CanaryLib::DataType_RawData);
+      auto content_msg = CanaryLib::GetContentMessage(body_buffer);
+    }
 
-    CanaryLib::FlatbuffersWrapper inputWrapper;
-    wrapper.Finish();
-    inputWrapper.copy(wrapper.Finish());
+    TEST_CASE("Reset") {
+      flatbuffers::FlatBufferBuilder fbb;
+      fbb.PreAlign(CanaryLib::WRAPPER_MAX_BODY_SIZE, 8);
+      CanaryLib::FlatbuffersWrapper wrapper1;
+      generateBaseWrapper(wrapper1);
+      wrapper1.Finish(&xtea);
+      validateBasic(wrapper1, &xtea);
 
-    // validate checksum
-    bool checksummed = inputWrapper.readChecksum();
+      wrapper1.reset();
+      CHECK(wrapper1.isEncryptionEnabled());
+      CHECK_FALSE(wrapper1.isWriteLocked());
+      CHECK_EQ(wrapper1.Size(), 0);
+      CHECK_EQ(wrapper1.Types().size(), 0);
+      CHECK_EQ(wrapper1.Contents().size(), 0);
+    }
 
-    auto enc_msg = inputWrapper.getEncryptedMessage();
-    uint8_t *body_buffer = (uint8_t *) enc_msg->body()->Data();
+    TEST_CASE("Finish") {
+      CanaryLib::FlatbuffersWrapper wrapper1;
+      generateBaseWrapper(wrapper1);
+      wrapper1.Finish();
+      validateBasic(wrapper1, nullptr);
 
-    auto content_msg = CanaryLib::GetContentMessage(body_buffer);
-  }
+      wrapper1.reset();
+      generateBaseWrapper(wrapper1);
+      wrapper1.Finish(&xtea);
+      validateBasic(wrapper1, &xtea);
+    }
 
-  TEST_CASE("Reset") {
-    flatbuffers::FlatBufferBuilder fbb;
-    fbb.PreAlign(CanaryLib::WRAPPER_MAX_BODY_SIZE, 8);
-    CanaryLib::FlatbuffersWrapper wrapper1;
-    generateBaseWrapper(wrapper1);
-    wrapper1.Finish(&xtea);
-    validateBasic(wrapper1, &xtea);
+    TEST_CASE("Add") {
+      CanaryLib::FlatbuffersWrapper wrapper1;
+      generateBaseWrapper(wrapper1);
+      CHECK_EQ(wrapper1.Types().size(), 3);
+      CHECK_EQ(wrapper1.Contents().size(), 3);
 
-    wrapper1.reset();
-    CHECK(wrapper1.isEncryptionEnabled());
-    CHECK_FALSE(wrapper1.isWriteLocked());
-    CHECK_EQ(wrapper1.Size(), 0);
-    CHECK_EQ(wrapper1.Types().size(), 0);
-    CHECK_EQ(wrapper1.Contents().size(), 0);
-  }
+      wrapper1.Finish();
+      CHECK_THROWS_AS(flatbuffers::FlatBufferBuilder &fbb = wrapper1.Builder(), std::domain_error);
+    }
 
-  TEST_CASE("Finish") {
-    CanaryLib::FlatbuffersWrapper wrapper1;
-    generateBaseWrapper(wrapper1);
-    wrapper1.Finish();
-    validateBasic(wrapper1, nullptr);
+    TEST_CASE("LoadSizeFromBuffer") {
+      CanaryLib::FlatbuffersWrapper wrapper1;
+      xtea.generateKey();
+      uint8_t buffer[128];
+      uint16_t size = 64000;
+      uint16_t newSize = 3241;
 
-    wrapper1.reset();
-    generateBaseWrapper(wrapper1);
-    wrapper1.Finish(&xtea);
-    validateBasic(wrapper1, &xtea);
-  }
-
-  TEST_CASE("Add") {
-    CanaryLib::FlatbuffersWrapper wrapper1;
-    generateBaseWrapper(wrapper1);
-    CHECK_EQ(wrapper1.Types().size(), 3);
-    CHECK_EQ(wrapper1.Contents().size(), 3);
-
-    wrapper1.Finish();
-    CHECK_THROWS_AS(flatbuffers::FlatBufferBuilder &fbb = wrapper1.Builder(), std::domain_error);
-  }
-
-  TEST_CASE("LoadSizeFromBuffer") {
-    CanaryLib::FlatbuffersWrapper wrapper1;
-    xtea.generateKey();
-    uint8_t buffer[128];
-    uint16_t size = 64000;
-    uint16_t newSize = 3241;
-
-    memcpy(buffer, &size, 2);
-    CHECK_EQ(wrapper1.loadSizeFromBuffer(buffer), size);
-    CHECK_EQ(wrapper1.Size(), size);
-    memcpy(buffer, &newSize, 2);
-    CHECK_EQ(wrapper1.loadSizeFromBuffer(buffer), newSize);
-    CHECK_EQ(wrapper1.Size(), newSize);
+      memcpy(buffer, &size, 2);
+      CHECK_EQ(wrapper1.loadSizeFromBuffer(buffer), size);
+      CHECK_EQ(wrapper1.Size(), size);
+      memcpy(buffer, &newSize, 2);
+      CHECK_EQ(wrapper1.loadSizeFromBuffer(buffer), newSize);
+      CHECK_EQ(wrapper1.Size(), newSize);
+    }
   }
 }
